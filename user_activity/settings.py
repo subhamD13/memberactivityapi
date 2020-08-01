3f2a5388a97fbbb65661e7a55f0819f776724f69
subham-saha <subhamsaha216@gmail.com>
2020-08-01 08:29:58 +0530

    Modified settings.py

--- user_activity/settings.py
+++ user_activity/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '14ii1fvwxm2c9(h)pgvc=8ifjcd4irk@#=5e)qz7p))ksip_ma'
+# SECRET_KEY = '14ii1fvwxm2c9(h)pgvc=8ifjcd4irk@#=5e)qz7p))ksip_ma'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['memberactivityapi.herokuapp.com']
 
 
 # Application definition
